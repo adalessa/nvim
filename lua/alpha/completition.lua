a9c14629bdb3edd1d9803b9a6e311f8ba256908e
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2022-01-21 20:47:06 +0100

    add completition for open buffers

--- lua/alpha/completition.lua
+++ lua/alpha/completition.lua
@@ -39,7 +39,19 @@ cmp.setup {
         { name = "nvim_lsp" },
         { name = "path" },
         { name = "luasnip" },
-        { name = "buffer", keyword_length = 5 },
+        {
+            name = "buffer",
+            keyword_length = 5,
+            option = {
+                get_bufnrs = function()
+                    local bufs = {}
+                    for _, win in ipairs(vim.api.nvim_list_wins()) do
+                        bufs[vim.api.nvim_win_get_buf(win)] = true
+                    end
+                    return vim.tbl_keys(bufs)
+                end
+            }
+        },
     },
 
     snippet = {
