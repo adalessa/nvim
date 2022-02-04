fe25ff4878c1544c0d7cb8cd08f75bea21f27a6d
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2022-02-04 08:29:17 +0100

    Organize plugins.lua file

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,7 +2,7 @@ vim.cmd [[packadd packer.nvim]]
 vim.cmd([[autocmd BufWritePost plugins.lua source <afile> | PackerCompile]])
 
 return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
+    use {'wbthomason/packer.nvim', opt = true}
 
     use {
         'ThePrimeagen/refactoring.nvim',
@@ -12,10 +12,15 @@ return require('packer').startup(function(use)
         }
     }
 
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-    use { 'nvim-treesitter/playground' }
-    use { 'nvim-treesitter/nvim-treesitter-refactor' }
-    use { 'nvim-treesitter/nvim-treesitter-textobjects' }
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate',
+        requires = {
+            'nvim-treesitter/playground',
+            'nvim-treesitter/nvim-treesitter-refactor',
+            'nvim-treesitter/nvim-treesitter-textobjects',
+        }
+    }
 
     use {
         'ThePrimeagen/harpoon',
@@ -28,26 +33,29 @@ return require('packer').startup(function(use)
     use 'tpope/vim-dispatch'
 
     -- Completition
-    use "hrsh7th/nvim-cmp"
-    use "hrsh7th/cmp-buffer"
-    use "hrsh7th/cmp-path"
-    use "hrsh7th/cmp-nvim-lua"
-    use "hrsh7th/cmp-nvim-lsp"
-    use "saadparwaiz1/cmp_luasnip"
-    use "onsails/lspkind-nvim"
-    use "hrsh7th/cmp-nvim-lsp-signature-help"
-    use "windwp/nvim-autopairs"
+    use {
+        "hrsh7th/nvim-cmp",
+        requires = {
+             "hrsh7th/cmp-buffer",
+             "hrsh7th/cmp-path",
+             "hrsh7th/cmp-nvim-lua",
+             "hrsh7th/cmp-nvim-lsp",
+             "saadparwaiz1/cmp_luasnip",
+             "onsails/lspkind-nvim",
+            "L3MON4D3/LuaSnip",
+        }
+    }
 
-    use "ray-x/lsp_signature.nvim"
+    use "windwp/nvim-autopairs"
 
     use "tjdevries/colorbuddy.nvim"
 
     use {
         'neovim/nvim-lspconfig',
         'williamboman/nvim-lsp-installer',
+        'hrsh7th/cmp-nvim-lsp-signature-help',
+        'ray-x/lsp_signature.nvim'
     }
-
-    use 'L3MON4D3/LuaSnip'
 
     use {
         'nvim-telescope/telescope.nvim',
@@ -55,20 +63,25 @@ return require('packer').startup(function(use)
             { 'nvim-lua/plenary.nvim'},
             { 'nvim-lua/popup.nvim' },
             { 'nvim-telescope/telescope-fzy-native.nvim' },
-            { 'kyazdani42/nvim-web-devicons' }
+            { 'kyazdani42/nvim-web-devicons' },
+            { 'nvim-telescope/telescope-file-browser.nvim' }
         }
     }
-    use "nvim-telescope/telescope-file-browser.nvim"
 
     use {
         'nvim-lualine/lualine.nvim',
         requires = {'kyazdani42/nvim-web-devicons', opt = true}
     }
 
-    use 'rcarriga/nvim-dap-ui'
-    use 'mfussenegger/nvim-dap'
-    use "Pocco81/DAPInstall.nvim"
-    use 'leoluz/nvim-dap-go'
+    use {
+        'leoluz/nvim-dap-go',
+        requires = {
+            'rcarriga/nvim-dap-ui',
+            'mfussenegger/nvim-dap',
+            "Pocco81/DAPInstall.nvim",
+            'leoluz/nvim-dap-go',
+        }
+    }
 
     use 'szw/vim-maximizer'
 
@@ -110,12 +123,13 @@ return require('packer').startup(function(use)
 
     use 'tpope/vim-tbone'
 
-    use 'tpope/vim-dadbod'
-    use 'kristijanhusak/vim-dadbod-ui'
+    use {
+        'tpope/vim-dadbod',
+        requires = {
+           'kristijanhusak/vim-dadbod-ui',
+        }
+    }
 
     use { 'nicwest/vim-http', opt = true, cmd = {'Http'}}
     use { 'vimwiki/vimwiki', opt = true, cmd = {'VimwikiIndex'}}
-
-    use 'ggandor/lightspeed.nvim'
-
 end)
