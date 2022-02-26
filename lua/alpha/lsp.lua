3ea96709fcd2c76200bdfb8e0de9739caa108d17
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2022-02-26 12:35:42 +0100

    set spaces for go, update lsp config for diagnostic

--- lua/alpha/lsp.lua
+++ lua/alpha/lsp.lua
@@ -53,15 +53,18 @@ lsp_installer.on_server_ready(function(server)
     vim.cmd [[ do User LspAttachBuffers ]]
 end)
 
-require "lspconfig".efm.setup {
-    init_options = {documentFormatting = true},
-    settings = {
-        rootMarkers = {".git/"},
-        languages = {
-            blade = {
-                {formatCommand = "blade-formatter --stdin", formatStdin = true}
-            },
-            json = {}
-        }
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        underline = true,
+        update_in_insert = false,
+        virtual_text = { spacing = 2, prefix = "●" },
+        severity_sort = true,
     }
-}
+)
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
