942137bfe8ffcfd94e6def10805b47f72a521a6d
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2022-03-01 07:30:36 +0100

    update db ui

--- after/plugin/dbui.lua
+++ after/plugin/dbui.lua
@@ -1,5 +1,5 @@
-vim.g.db_ui_show_database_icon = 1
 vim.g.db_ui_use_nerd_fonts = 1
+vim.g.db_ui_show_database_icon = 1
 vim.g.db_ui_force_echo_notifications = 1
 
 vim.g.db_ui_table_helpers = {
@@ -7,6 +7,34 @@ vim.g.db_ui_table_helpers = {
         Count = "select count(1) from {optional_schema}{table}",
         Explain = "EXPLAIN {last_query}"
     }
+}
+
+vim.g.db_ui_icons = {
+    expanded = {
+        db = '▾ ',
+        buffers = '▾ ',
+        saved_queries = '▾ ',
+        schemas = '▾ ',
+        schema = '▾ פּ',
+        tables = '▾ 藺',
+        table = '▾ ',
+    },
+    collapsed = {
+        db = '▸ ',
+        buffers = '▸ ',
+        saved_queries = '▸ ',
+        schemas = '▸ ',
+        schema = '▸ פּ',
+        tables = '▸ 藺',
+        table = '▸ ',
+    },
+    saved_query = '',
+    new_query = '璘',
+    tables = '離',
+    buffers = '﬘',
+    add_connection = '',
+    connection_ok = '✓',
+    connection_error = '✕',
 }
 
 -- opening it in a new tab
