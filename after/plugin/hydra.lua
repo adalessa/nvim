f61af678dd5da523340c71f57eb95d8d8a76d780
Ariel D'Alessandro <ariel.dalessandro@auto1.com>
2022-08-05 09:15:30 +0200

    try new hydra for make, add snippets for behat

--- after/plugin/hydra.lua
+++ after/plugin/hydra.lua
@@ -1,28 +1,45 @@
 local ok, hydra = pcall(require, "hydra")
 if not ok then
-    return
+	return
 end
 
 -- Windows management, move and resize
 hydra({
-    name = "windows",
-    mode = "n",
-    body = "<leader>w",
-    heads = {
-        { 'l', '<C-w>l' },
-        { 'h', '<C-w>h' },
-        { 'j', '<C-w>j' },
-        { 'k', '<C-w>k' },
-        { 'q', '<C-w>q' },
-        { 'o', '<C-w>o' },
-        { 'L', '<C-w>2>' },
-        { 'H', '<C-w>2<' },
-        { 'J', '<C-w>2-' },
-        { 'K', '<C-w>2+' },
-        { '<C-l>', '<C-w>L' },
-        { '<C-h>', '<C-w>H' },
-        { '<C-j>', '<C-w>J' },
-        { '<C-k>', '<C-w>K' },
-        { '=', '<C-w>=' },
-    },
+	name = "windows",
+	mode = "n",
+	body = "<C-w>",
+	heads = {
+		{ "l", "<C-w>l" },
+		{ "h", "<C-w>h" },
+		{ "j", "<C-w>j" },
+		{ "k", "<C-w>k" },
+		{ "q", "<C-w>q" },
+		{ "o", "<C-w>o" },
+		{ "L", "<C-w>2>" },
+		{ "H", "<C-w>2<" },
+		{ "J", "<C-w>2-" },
+		{ "K", "<C-w>2+" },
+		{ "<C-l>", "<C-w>L" },
+		{ "<C-h>", "<C-w>H" },
+		{ "<C-j>", "<C-w>J" },
+		{ "<C-k>", "<C-w>K" },
+		{ "=", "<C-w>=" },
+	},
+})
+
+-- want a way to run make commands
+-- Make commands runs good but the output is not good
+-- like up I like that is done and good
+hydra({
+	name = "make",
+	mode = "n",
+	body = "<C-m>",
+	heads = {
+		{ "t", function() vim.cmd("vsplit term://make test") end },
+        { "v", function()
+            vim.cmd('vert new')
+            vim.fn.termopen("make vars")
+            vim.cmd('startinsert')
+        end },
+	},
 })
---
+++ lua/alpha/snips/ft/cucumber.lua
@@ -0,0 +1,9 @@
+
+local ls = require("luasnip")
+local t = ls.text_node
+
+local M = {
+    clean = t([[Given clean stubs list]]),
+}
+
+return M
