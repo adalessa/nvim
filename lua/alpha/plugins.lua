964698e302a9aefb77d6cfe3ce2a3ee93e87d24c
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2022-09-21 19:34:41 +0200

    Add mind

--- lua/alpha/plugins.lua
+++ lua/alpha/plugins.lua
@@ -224,4 +224,13 @@ return require("packer").startup(function(use)
 	})
 
 	use({ "xiyaowong/nvim-transparent" })
+
+	use({
+		"phaazon/mind.nvim",
+		branch = "v2.2",
+		requires = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("mind").setup()
+		end,
+	})
 end)
