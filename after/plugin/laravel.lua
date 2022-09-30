bc7ded2a1cebcd4bbf5e549b107c7e11a5f32535
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2022-09-30 21:08:37 +0200

    feat: set cucumber for phpactor

--- after/plugin/laravel.lua
+++ after/plugin/laravel.lua
@@ -3,9 +3,12 @@ if not ok then
 	return
 end
 
+local artisan = require("laravel.artisan")
+local notify = require("notify")
+
 laravel.setup()
 
 vim.keymap.set("n", "<leader>ac", function()
-	require("laravel.artisan").clean_cmd_list_cache()
-	require("notify").notify("Artisan cmd list cache clean", "info", { title = "Laravel.nvim" })
+	artisan.clean_cmd_list_cache()
+	notify.notify("Artisan cmd list cache clean", "info", { title = "Laravel.nvim" })
 end, { noremap = true })
