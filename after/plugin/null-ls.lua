b52a4e554f54e5a374febcd069a2e4b9c0d81e4c
Ariel D'Alessandro <ariel.dalessandro@auto1.com>
2022-10-04 14:42:36 +0200

    set on save for the diagnostic sources

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -28,6 +28,7 @@ local laravel_sources = {
 	null_ls.builtins.diagnostics.phpstan.with({
 		command = "./vendor/bin/phpstan",
         to_temp_file = false,
+        method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
 	}),
 	null_ls.builtins.formatting.pint,
 	null_ls.builtins.formatting.blade_formatter,
@@ -37,9 +38,11 @@ local symonfy_sources = {
 	null_ls.builtins.diagnostics.phpstan.with({
         command = "./bin/phpstan",
         to_temp_file = false,
+        method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
     }),
 	null_ls.builtins.diagnostics.phpcs.with({
         command = "./bin/phpcs",
+        method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
     }),
 }
 
