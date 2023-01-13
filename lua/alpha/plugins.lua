0586c7a92cc352925e0e60009dd4ec975880748c
Ariel D'Alessandro <ariel.dalessandro@auto1.com>
2023-01-13 10:43:22 +0100

    feat: move harpoon to lazy load

--- lua/alpha/plugins.lua
+++ lua/alpha/plugins.lua
@@ -65,6 +65,10 @@ return require("packer").startup(function(use)
 			"nvim-lua/plenary.nvim",
 			"nvim-lua/popup.nvim",
 		},
+		keys = require('alpha.harpoon').keymaps,
+		config = function ()
+			require('alpha.harpoon').setup()
+		end
 	})
 
 	use({
