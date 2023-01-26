e44f56e76ffe3d8fcf2e2ef7eb5beb739969a1b5
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2023-01-26 22:57:07 +0100

    fix: autocmd for keybindings

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,14 +1,12 @@
 require("config.options")
+-- autocmds and keymaps can wait to load
+vim.api.nvim_create_autocmd("User", {
+    group = vim.api.nvim_create_augroup("LazyVim", { clear = true }),
+    pattern = "VeryLazy",
+    callback = function()
+        require("config.autocmds")
+        require("config.keymaps")
+        require("config.diagnostic")
+    end,
+})
 require("config.lazy")
-if vim.fn.argc() == 0 then
-    -- autocmds and keymaps can wait to load
-    vim.api.nvim_create_autocmd("User", {
-        group = vim.api.nvim_create_augroup("LazyVim", { clear = true }),
-        pattern = "VeryLazy",
-        callback = function()
-            require("config.autocmds")
-            require("config.keymaps")
-            require("config.diagnostic")
-        end,
-    })
-end
