870d3cfae760265c01403deb2b973e500fda06df
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2023-02-03 19:26:29 +0100

    feat: add snippet to neogen

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -2,7 +2,5 @@ return {
   "danymat/neogen",
   dependencies = "nvim-treesitter/nvim-treesitter",
   cmd = { "Neogen" },
-  config = true,
-  -- Uncomment next line if you want to follow only stable versions
-  -- version = "*"
+  opts = { snippet_engine = "luasnip" },
 }
