234d94a3613083eb478457eb800dace438d0df3c
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2023-02-18 10:27:49 +0100

    feat: add bash, add color picker

---
+++ lua/plugins/color-picker.lua
@@ -0,0 +1,7 @@
+return {
+  "ziontee113/color-picker.nvim",
+  keys = {
+    {"<C-c>", "<cmd>PickColorInsert<cr>", mode = "i"},
+  },
+  config = true,
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,6 +9,7 @@ return {
   config = function()
     require("nvim-treesitter.configs").setup {
       ensure_installed = {
+        "bash",
         "css",
         "go",
         "gomod",
