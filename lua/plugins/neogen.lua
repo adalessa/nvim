8bae7bee6dc46ab0822d85495868b1f8605066b5
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2023-03-10 08:38:55 +0100

    feat: add keymap to neogen

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -2,5 +2,6 @@ return {
   "danymat/neogen",
   dependencies = "nvim-treesitter/nvim-treesitter",
   cmd = { "Neogen" },
+  keys = {{"<leader>n", ":Neogen<cr>"}},
   opts = { snippet_engine = "luasnip" },
 }
