28e93e84d953465c413491dc326cf44f2444e1f0
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2023-04-11 19:43:25 +0200

    feat(laravel): udpate with latest plugin structure

--- lua/plugins/laravel.lua
+++ lua/plugins/laravel.lua
@@ -8,6 +8,14 @@ return {
   keys = {
     { "<leader>la", ":Laravel artisan<cr>", desc = "Laravel Application Commands" },
     { "<leader>lr", ":Laravel routes<cr>", desc = "Laravel Application Routes" },
+    {
+      "<leader>lt",
+      function()
+        require("laravel").app.sendToTinker()
+      end,
+      mode = "v",
+      desc = "Laravel Application Routes",
+    },
   },
   config = function()
     vim.g.laravel_log_level = "debug"
