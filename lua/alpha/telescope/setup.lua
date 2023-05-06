263316603286bb2b3cefe5e6bd8791788f5685b1
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2023-05-06 17:15:21 +0200

    Change layout depending on the amount of lines

--- lua/alpha/telescope/setup.lua
+++ lua/alpha/telescope/setup.lua
@@ -30,7 +30,7 @@ require("telescope").setup {
           if cols > 200 then
             return math.floor(cols * 0.5)
           else
-            return math.floor(cols * 0.6)
+            return math.floor(cols * 0.3)
           end
         end,
       },
