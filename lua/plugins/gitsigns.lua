72c8f0deb1e8b3f069076a317d1f2d8abd97fae4
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2023-08-31 07:11:01 +0200

    remove no longer need dependency

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,23 +1,18 @@
 return {
   "lewis6991/gitsigns.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
+  opts = {
+    signcolumn = false,
+    numhl = true,
+    trouble = false,
+    on_attach = function(bufnr)
+      local gitsigns = require "gitsigns"
+      vim.keymap.set("n", "<leader>hs", gitsigns.stage_hunk, { buffer = bufnr })
+      vim.keymap.set("n", "<leader>hu", gitsigns.undo_stage_hunk, { buffer = bufnr })
+      vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk, { buffer = bufnr })
+      vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk, { buffer = bufnr })
+      vim.keymap.set("n", "<leader>hj", gitsigns.next_hunk, { buffer = bufnr })
+      vim.keymap.set("n", "<leader>hk", gitsigns.prev_hunk, { buffer = bufnr })
+    end,
+    max_file_length = 10000,
   },
-  config = function()
-    local gitsigns = require "gitsigns"
-    gitsigns.setup {
-      signcolumn = false,
-      numhl = true,
-      trouble = false,
-      on_attach = function(bufnr)
-        vim.keymap.set("n", "<leader>hs", gitsigns.stage_hunk, { buffer = bufnr })
-        vim.keymap.set("n", "<leader>hu", gitsigns.undo_stage_hunk, { buffer = bufnr })
-        vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk, { buffer = bufnr })
-        vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk, { buffer = bufnr })
-        vim.keymap.set("n", "<leader>hj", gitsigns.next_hunk, { buffer = bufnr })
-        vim.keymap.set("n", "<leader>hk", gitsigns.prev_hunk, { buffer = bufnr })
-      end,
-      max_file_length = 10000,
-    }
-  end,
 }
